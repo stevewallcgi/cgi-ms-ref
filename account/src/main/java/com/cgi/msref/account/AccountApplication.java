982da59d6228c640ae95a7e2c07116b35d34880c
Steve Wall <swall@ecsteam.com>
2018-03-30 09:14:35 -0600

    Creating account-client

--- account/src/main/java/com/cgi/msref/account/AccountApplication.java
+++ account/src/main/java/com/cgi/msref/account/AccountApplication.java
@@ -2,8 +2,10 @@ package com.cgi.msref.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class AccountApplication {
 
 	public static void main(String[] args) {
