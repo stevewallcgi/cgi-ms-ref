a7c443375136481d7c4b8b81713d1810dc1a6166
Steve Wall <swall@ecsteam.com>
2018-04-02 08:27:19 -0600

    WIP - Adding Hystrix

--- account-client/src/main/java/com/cgi/msref/accountclient/AccountClientApplication.java
+++ account-client/src/main/java/com/cgi/msref/accountclient/AccountClientApplication.java
@@ -2,8 +2,10 @@ package com.cgi.msref.accountclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class AccountClientApplication {
 
 	public static void main(String[] args) {
